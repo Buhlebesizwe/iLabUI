b5eb2f21c3c9453871bc76e5d80c330a78c0de9c
sizwe shongwe <shongwebm8@gmail.com>
2022-07-12 12:02:38 +0200

    final

--- src/test/java/TestRunner/JUnitTestRunner.java
+++ src/test/java/TestRunner/JUnitTestRunner.java
@@ -1,5 +1,5 @@
 package TestRunner;
-import com.cucumber.listener.Reporter;
+
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -13,8 +13,6 @@ import java.io.File;
         "json:target/test_reports/json/cucumber.jason"})
 
 public class JUnitTestRunner {
-    public static void writeExtentReport() {
-        Reporter.loadXMLConfig(new File("config/report.xml"));
-    }
+
 
 }
